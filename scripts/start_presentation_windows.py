dfdecaecdd223249e0c82ab5987cee5a3dccc647
Marco <viaimperiale@gmail.com>
2021-04-09 11:28:23 +0200

    deployed

--- scripts/start_presentation_windows.py
+++ scripts/start_presentation_windows.py
@@ -1,3 +1,10 @@
+import os
 from subprocess import run
 
-run(["wsl", "-d", "Ubuntu-20.04", "bash", "-c", "-i", "tv"])
+try:
+    working_proj_dir = os.path.join(os.path.dirname(__file__))
+    os.startfile(os.path.join(working_proj_dir, "pagina presentazione.lnk"))
+    run(["wsl", "-d", "Ubuntu-20.04", "bash", "-c", "-i", "tv"])
+except Exception as e:
+    print(e)
+    input()
